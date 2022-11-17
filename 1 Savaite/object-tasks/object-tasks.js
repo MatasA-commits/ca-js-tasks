e97880fdc8fbcf4f3fa2b6da2ca34589bc85d7ac
MatasA <aleliunasmatas@gmail.com>
2022-11-17 14:08:01 +0200

    Object-tasks task 1 finished

---
+++ 1 Savaite/object-tasks/object-tasks.js
@@ -0,0 +1,56 @@
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+{
+  drinks = [
+    {name: "lemonade", price: 50},
+    {name: "lime", price: 10}
+  ]
+  function sortDrinkByPrice(drinks) {
+   return drinks.sort((a, b) => (a.price > b.price) ? 1 : -1);
+  }
+  
+  console.log(sortDrinkByPrice(drinks));
+   
+}
+console.groupEnd();
+
+console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
+{
+  
+}
+console.groupEnd();
+
+console.groupCollapsed('3 - https://edabit.com/challenge/48EJWLhF224na8po3');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('4 - https://edabit.com/challenge/i6YqzHcSiPiEQKjeX');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('5 - https://edabit.com/challenge/8s2jy9hR2TAeQinKD');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('6 - https://edabit.com/challenge/pPNAs5PvB3WvnDwDM');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('7 - https://edabit.com/challenge/QXWM2oo7rQNiyDsip');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('8 - https://edabit.com/challenge/pLNavsePxJ87t9Nak');
+{
+
+}
+console.groupEnd();
