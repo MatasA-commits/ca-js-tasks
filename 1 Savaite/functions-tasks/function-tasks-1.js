92ef7f8aef8901acc9fdcf012a20cf1c7496b98f
MatasA <aleliunasmatas@gmail.com>
2022-11-18 11:28:07 +0200

    task-1 finished

--- 1 Savaite/functions-tasks/function-tasks-1.js
+++ 1 Savaite/functions-tasks/function-tasks-1.js
@@ -1,48 +1,106 @@
 console.groupCollapsed('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  function getArrayFirstElement(array){
+    const firstElement = array[0];
+    return firstElement;
+  }
+  // bandomieji kintamieji
+  const array1 = [2, 4, 10, 14];
+  const array2 = ['hello', 'how', 'are', 'you'];
+  const array3 = ['%', '$', '@'];
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+  const callArray1 = getArrayFirstElement(array1);
+  const callArray2 = getArrayFirstElement(array2);
+  const callArray3 = getArrayFirstElement(array3)
+  // rezultatų spausdinimas
+  console.log(callArray1);
+  console.log(callArray2);
+  console.log(callArray3);
 }
 console.groupEnd();
 
 console.groupCollapsed('2. Parašykite funkciją, kuri pašalina pirmą masyvo elementą ir jį grąžina.');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('3. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('4. Parašykite funkciją, kuri pašalina paskutinį masyvo elementą ir jį grąžina.');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('5. Parašykite funkciją, kuri grąžina elementų kiekį masyve');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('6. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus eilutėmis');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes eilutėmis');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
@@ -51,31 +109,61 @@ console.groupCollapsed('9. Parašykite funkciją, kuri atspausdina kiekvieno mas
 // [1] => reikšmė
 // [2] => reikšmė
 {
-  // ... sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('10. Parašykite funkciją, kuri atspausdina masyvo elementus atbuline tvarka eilutėmis, iš galo.');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('11. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('12. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48');
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
 
 console.groupCollapsed('13. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
 //  [0]=>17 [1]=>8 [2]=>88 ..
 {
-  // ...sprendimas ir spausdinimas
+  // funkcijos deklaracija
+  
+  // bandomieji kintamieji
+
+  // funkcijos iškvietimai ir saugojimai į rezultatus
+
+  // rezultatų spausdinimas
 }
 console.groupEnd();
