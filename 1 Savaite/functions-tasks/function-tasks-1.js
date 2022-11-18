505138117a9d3d6c69121fd7c872d99fcbfd9b51
MatasA <aleliunasmatas@gmail.com>
2022-11-18 10:40:46 +0200

    function-tasks creation

---
+++ 1 Savaite/functions-tasks/function-tasks-1.js
@@ -0,0 +1,81 @@
+console.groupCollapsed('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('2. Parašykite funkciją, kuri pašalina pirmą masyvo elementą ir jį grąžina.');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('3. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('4. Parašykite funkciją, kuri pašalina paskutinį masyvo elementą ir jį grąžina.');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('5. Parašykite funkciją, kuri grąžina elementų kiekį masyve');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('6. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus eilutėmis');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes eilutėmis');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('9. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras eilutėmis, tokiu formatu:  ');
+// [0] => reikšmė
+// [1] => reikšmė
+// [2] => reikšmė
+{
+  // ... sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('10. Parašykite funkciją, kuri atspausdina masyvo elementus atbuline tvarka eilutėmis, iš galo.');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('11. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('12. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('13. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
+//  [0]=>17 [1]=>8 [2]=>88 ..
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
