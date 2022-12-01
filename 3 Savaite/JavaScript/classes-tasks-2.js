f50fc6af81cabb06df35d10fb482e43da1fa8542
MatasA <aleliunasmatas@gmail.com>
2022-12-01 13:34:20 +0200

    classes-tasks-2 initalized, first task finished

---
+++ 3 Savaite/JavaScript/classes-tasks-2.js
@@ -0,0 +1,86 @@
+console.groupCollapsed('1. - https://edabit.com/challenge/ZngT4zDckDugt2JGY');
+{
+  class Player {
+    constructor(name, age, height, weight) {
+      this.name = name;
+      this.age = age;
+      this.height = height;
+      this.weight = weight;
+    }
+
+    getAge() {
+      return `${this.name} is age ${this.age}`;
+    }
+
+    getHeight() {
+      return `${this.name} is ${this.height}cm`;
+    }
+
+    getWeight() {
+      return `${this.name} weighs ${this.weight}kg`;
+    }
+  }
+  p1 = new Player("David Jones", 25, 175, 75);
+  const getAge = p1.getAge();
+  console.log(getAge);
+  const getHeight = p1.getHeight();
+  console.log(getHeight);
+  const getWeight = p1.getWeight();
+  console.log(getWeight);
+  console.groupEnd();
+}
+console.groupEnd();
+
+console.groupCollapsed('2. - https://edabit.com/challenge/yxKoCKemzacK6PECM');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('3. - https://edabit.com/challenge/kGLhgwGaLJsCMS7wS');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('4. - https://edabit.com/challenge/iwdZiFucR5wkQsFHu');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('5. - https://edabit.com/challenge/Hgb38yhWGwJCMHbRQ');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('6. - https://edabit.com/challenge/qNMtrtizgssAQqP2b');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('7. - https://edabit.com/challenge/HKmJFmZZCX53ff4ke');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('8. - https://edabit.com/challenge/9zwdrfW99zmdRhibi');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('9. - https://edabit.com/challenge/7PA4jhWqDYJT4ixLp');
+{
+  // ... code
+}
+console.groupEnd();
+
+console.groupCollapsed('10. - https://edabit.com/challenge/s5Sz2ovKsvtGxNGgn');
+{
+  // ... code
+}
+console.groupEnd();
