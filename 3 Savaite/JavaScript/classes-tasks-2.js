a77e915559f37f302e4ed5b8457f03be5358861f
MatasA <aleliunasmatas@gmail.com>
2022-12-02 12:59:15 +0200

    feat: tasks 5-6 finished

--- 3 Savaite/JavaScript/classes-tasks-2.js
+++ 3 Savaite/JavaScript/classes-tasks-2.js
@@ -92,13 +92,67 @@ console.groupEnd();
 
 console.groupCollapsed('4. - https://edabit.com/challenge/iwdZiFucR5wkQsFHu');
 {
-  // ... code
+  class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    compareAge(other) {
+      if (this.age < other.age) return `${other.name} is older than me.`;
+      else if (this.age > other.age) return `${other.name} is younger than me.`;
+      else return `${other.name} is the same age as me.`;
+    }
+  }
+
+  p1 = new Person("Samuel", 24)
+  p2 = new Person("Joel", 36)
+  p3 = new Person("Lily", 24)
+
+  console.log(p1.compareAge(p2));
+  console.log(p2.compareAge(p1));
+  console.log(p1.compareAge(p3));
+
+
 }
 console.groupEnd();
 
 console.groupCollapsed('5. - https://edabit.com/challenge/Hgb38yhWGwJCMHbRQ');
 {
-  // ... code
+  class Rectangle {
+    constructor(sideA, sideB) {
+      this.sideA = sideA
+      this.sideB = sideB
+    }
+    getArea(){return this.sideA*this.sideB}
+    getPerimeter(){return (this.sideA + this.sideB) *2}
+  }
+  
+  
+  class Circle {
+  static pi = 3.14159265359;
+    constructor(r){
+      this.r = r;
+      
+    }
+    
+    getArea(){
+      return Circle.pi * this.r ** 2;
+    }
+
+    getPerimeter(){
+      return 2 * Circle.pi * this.r;
+    }
+
+  }
+  
+  
+  // unquote and use run to test these cases:
+  
+  let q = new Circle(4.44);
+  console.log(q.getArea());
+  console.log(q.getPerimeter());
+  
 }
 console.groupEnd();
 
