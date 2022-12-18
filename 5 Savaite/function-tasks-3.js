73146ce59577886c28b7439fd6f29cd7037fe54b
MatasA <aleliunasmatas@gmail.com>
2022-12-18 13:07:08 +0200

    function-tasks-3 finished, js-revision initiated

--- 5 Savaite/function-tasks-3.js
+++ 5 Savaite/function-tasks-3.js
@@ -1,48 +1,49 @@
-console.group("28. Sukurkite funkciją, kuri atspausdina žodį atvirkščiai 'abc' -> 'cba'");
+console.group(
+  "28. Sukurkite funkciją, kuri atspausdina žodį atvirkščiai 'abc' -> 'cba'"
+);
 {
   function strReverse(str) {
     let stringArray = [];
-    for(let i = 0; i < str.length; i += 1){
+    for (let i = 0; i < str.length; i += 1) {
       stringArray.unshift(str[i]);
     }
-    console.log(stringArray.join(''))
-    
+    console.log(stringArray.join(""));
   }
-   console.log('---');
-   strReverse('viens du trys');
-   strReverse('as tave myliu');
-   strReverse('Bairis seniuk');
-   console.log('---');
+  console.log("---");
+  strReverse("viens du trys");
+  strReverse("as tave myliu");
+  strReverse("Bairis seniuk");
+  console.log("---");
 }
 console.groupEnd();
 console.log();
-
 
 // 29. Sukurkite funkciją, kuri grąžina <true>, jeigu žodis yra palindromas (taip pat skaitosi iš abiejų pusių)
 //  mama -> false
 //  mamam -> true
 //  123321 -> true
 //  123456 -> false
-console.group("29. Sukurkite funkciją, kuri grąžina <true>, jeigu žodis yra palindromas (taip pat skaitosi iš abiejų pusių)");
+console.group(
+  "29. Sukurkite funkciją, kuri grąžina <true>, jeigu žodis yra palindromas (taip pat skaitosi iš abiejų pusių)"
+);
 {
   function isPalyndrome(str) {
     let stringArray = [];
-    for(let i = 0; i < str.length; i += 1){
+    for (let i = 0; i < str.length; i += 1) {
       stringArray.unshift(str[i]);
     }
-    const reverseString = stringArray.join('').toLowerCase();
-    if(str === reverseString) return true;
+    const reverseString = stringArray.join("").toLowerCase();
+    if (str === reverseString) return true;
     else return false;
-    
   }
-   console.log('---');
-   console.log({
-     'mama': isPalyndrome('mama'),
-     'mamam': isPalyndrome('mamam'),
-     '123321': isPalyndrome('123321'),
-     '123456': isPalyndrome('123456'),
-   })
-   console.log('---');
+  console.log("---");
+  console.log({
+    mama: isPalyndrome("mama"),
+    mamam: isPalyndrome("mamam"),
+    123321: isPalyndrome("123321"),
+    123456: isPalyndrome("123456"),
+  });
+  console.log("---");
 }
 console.groupEnd();
 console.log();
@@ -54,9 +55,8 @@ console.log();
 //    mėgstu pieną , bet medų nelabai.labanakt. -> Mėgstu pieną, bet medų nelabai. Labanakt.
 console.group("30. Sukurkite funkciją, kuri taiso pastraipos klaidas");
 {
-
   function capitalizeFirstLetter(str) {
-    // code ...
+    return str[0].toUpperCase() + str.slice(1);
   }
 
   function splitIntoSentences(paragraph) {
@@ -77,63 +77,65 @@ console.group("30. Sukurkite funkciją, kuri taiso pastraipos klaidas");
 
   // 1.
   function fixParagraph(paragraph) {
-    /*
-      2. Pirmiausiai suskirstome į sakinius, naudojant splitIntoSentences: 
-            string  ->  {
-                          sentences: [sentence1, sentence2, ... , sentenceN],
-                          separator: [ '.', '?', ... , '!'],
-                        }
-      3. Redaguoti kiekvieną sakinį, naudojant funkciją reduceEmptySpaces
-      4. Redaguoti kiekvieną sakinį, naudojant funkciją capitalizeFirstLetter
-      5. Sujungti sakinius su atitinkamais sakinių skiriamaisiais/baigiamaisiais ženklais
-      6. Grąžinti rezultatą.
-    */
+    return paragraph
+      .split(/([.?!])/)
+      .slice(0, -1)
+      .map((sentence) => sentence.trim())
+      .map(capitalizeFirstLetter)
+      .join("")
+      .replaceAll(/\s+/g, " ")
+      .replaceAll(" ,", ",")
+      .replaceAll(/([.?!,])([^\s])/g, "$1 $2");
   }
 
-  // const paragraph = '    labas , as jonas . Tave      vadina Kęstu? Taip ir žinojau  !    ';
-  // const fixedParagraph = fixParagraph(paragraph);
-  // console.log(paragraph);
-  // console.log(fixedParagraph);
-  // console.log('---');
+  const paragraph =
+    "    labas , as jonas . Tave      vadina Kęstu? Taip ir žinojau  !    ";
+  const fixedParagraph = fixParagraph(paragraph);
+  console.log(paragraph);
+  console.log(fixedParagraph);
+  console.log("---");
 }
 console.groupEnd();
 console.log();
 
 // 31. Sukurkite funkciją, kuri pirmu parametru priima sakinį ir sudeda atskirus sakinio žodžius į masyvą:
 //  labas, aš esu grybas -> ['labas', 'aš', 'esu', 'grybas']
-console.group("31. Sukurkite funkciją, kuri pirmu parametru priima sakinį ir sudeda atskirus sakinio žodžius į masyvą");
+console.group(
+  "31. Sukurkite funkciją, kuri pirmu parametru priima sakinį ir sudeda atskirus sakinio žodžius į masyvą"
+);
 {
   function splitSentence(sentence) {
-    const splitedStringArray = sentence.split(' ');
+    const splitedStringArray = sentence.split(" ");
     return splitedStringArray;
   }
-   console.log('---');
-   console.log({
-     'Labas aš Jonas': splitSentence('Labas aš Jonas'),
-     'Kėgliai yra gerai': splitSentence('Kėgliai yra gerai'),
-   })
-   console.log('---');
+  console.log("---");
+  console.log({
+    "Labas aš Jonas": splitSentence("Labas aš Jonas"),
+    "Kėgliai yra gerai": splitSentence("Kėgliai yra gerai"),
+  });
+  console.log("---");
 }
 console.groupEnd();
 console.log();
 
-
 // 32. Sukurkite funkciją, kuri pirmu parametru priimą <string>'ą, o antruoju skiriklį
 //  Ši funkcija turi grąžinti masyvą sudarytą iš atskirtų dalių:
-//  explode('Man patinka sniegas', ' ');  -> ['man', 'patinka', 'sniegas'];  
-//  explode('home/about/13', '/');        -> ['home', 'about', '13'];  
-console.group("32. Sukurkite funkciją, kuri pirmu parametru priimą <string>'ą, o antruoju skiriklį");
+//  explode('Man patinka sniegas', ' ');  -> ['man', 'patinka', 'sniegas'];
+//  explode('home/about/13', '/');        -> ['home', 'about', '13'];
+console.group(
+  "32. Sukurkite funkciją, kuri pirmu parametru priimą <string>'ą, o antruoju skiriklį"
+);
 {
   function explode(str, separator) {
     const splitedStringArray = str.split(separator);
     return splitedStringArray;
   }
-   console.log('---');
-   console.log({
-     "'Man patinka sniegas', ' '": explode('Man patinka sniegas', ' '),
-     "'home/about/13', '/'": explode('home/about/13', '/'),
-   })
-   console.log('---');
+  console.log("---");
+  console.log({
+    "'Man patinka sniegas', ' '": explode("Man patinka sniegas", " "),
+    "'home/about/13', '/'": explode("home/about/13", "/"),
+  });
+  console.log("---");
 }
 console.groupEnd();
 console.log();
